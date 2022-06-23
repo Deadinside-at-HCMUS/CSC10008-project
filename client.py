92f13d875d610ba13034f5374ac3f84a7917b135
Tointech <leanhthunk@gmail.com>
2022-06-23 19:41:05 +0700

    change to light theme

--- client.py
+++ client.py
@@ -9,25 +9,23 @@ BUFFER_SIZE = 1024
 MESSAGE = 'Hello, World!'
 
 if __name__ == '__main__':
-    # try:
-    #     client = socket.socket()
-    # except socket.error as s_e:
-    #     print("Error creating socket: ", s_e)
-    #     sys.exit(1)
+    try:
+        client = socket.socket()
+    except socket.error as s_e:
+        print("Error creating socket: ", s_e)
+        sys.exit(1)
 
-    # try:
-    #     client.connect((TCP_IP, TCP_PORT))
-    #     print(f"Connected to server {TCP_IP} in port {TCP_PORT}")
-    # except socket.gaierror as s_g:
-    #     print("Address-related error connecting to server: ", s_g)
-    #     sys.exit(1)
-    # except socket.error as s_e:
-    #     print("Error: ", s_e)
-
-    # try:
+    try:
+        client.connect((TCP_IP, TCP_PORT))
+        print(f"Connected to server {TCP_IP} in port {TCP_PORT}")
         Login.page()
-    #     BYT = MESSAGE.encode()
-    #     client.send(BYT)
-    #     print("Send message!")
-    # finally:
-    #     client.close()
+        BYT = MESSAGE.encode()
+        client.send(BYT)
+        print("Send message!")
+    except socket.gaierror as s_g:
+        print("Address-related error connecting to server: ", s_g)
+        sys.exit(1)
+    except socket.error as s_e:
+        print("Error: ", s_e)
+    finally:
+        client.close()
