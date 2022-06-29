591e39d91243ac44b78c18d94b8cbe4e1d7fab6a
Tointech <leanhthunk@gmail.com>
2022-06-29 11:53:30 +0700

    signup and login with UI

--- client.py
+++ client.py
@@ -1,31 +1,206 @@
 import socket
 import sys
-import Login
+import threading
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from signup import SignUp
 
 # Client set up
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = 'Hello, World!'
+IP = '127.0.0.1'
+PORT = 5005
+BUFFER_SIZE = 2048
+FORMAT = 'ascii'
 
-if __name__ == '__main__':
-    try:
-        client = socket.socket()
-    except socket.error as s_e:
-        print("Error creating socket: ", s_e)
-        sys.exit(1)
+# Colors
+WHITE = '#ffffff'
+DARK = '#000000'
+LIGHT = '#b5e2ff'
+BLUE = '#3047ff'
+LIGHT_GRAY = '#bdb9b1'
+DARK_GRAY = '#4f4e4d'
 
-    try:
-        client.connect((TCP_IP, TCP_PORT))
-        print(f"Connected to server {TCP_IP} in port {TCP_PORT}")
-        Login.page()
-        BYT = MESSAGE.encode()
-        client.send(BYT)
-        print("Send message!")
-    except socket.gaierror as s_g:
-        print("Address-related error connecting to server: ", s_g)
-        sys.exit(1)
-    except socket.error as s_e:
-        print("Error: ", s_e)
-    finally:
-        client.close()
+class Client():
+    def __init__(self, host, port):
+        # SOCKET CONNECTION
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
+
+        self.gui_done = False
+        self.running = True
+
+        gui_thread = threading.Thread(target=self.gui_loop)
+
+        gui_thread.start()
+
+    def gui_loop(self):
+        self.root = Tk()
+        # Rename window title
+        self.root.title('Login Page')
+        # Changing window icon
+        icon = ImageTk.PhotoImage(file='./images/cat.ico')
+        self.root.iconphoto(False, icon)
+
+        # Set window size
+        self.root.geometry('1166x718')
+        
+        # Set button close, minimize button beside x button
+        isResize = False
+        self.root.resizable(isResize, isResize)
+
+        # Background image
+        self.bg_frame = Image.open('./images/backgroundB.jpg')
+        photo = ImageTk.PhotoImage(self.bg_frame)
+        self.bg_panel = Label(self.root, image=photo)
+        self.bg_panel.image = photo
+        self.bg_panel.pack(fill='both', expand='yes')
+
+        # Login frame
+        self.lgn_frame = Frame(self.root, bg=WHITE, width=950, height=600)
+        self.lgn_frame.place(x=110, y=60)
+
+        self.txt = 'Welcome to E-Note!'
+        self.heading = Label(self.lgn_frame, text=self.txt, 
+                             font=('yu gothic ui', 25, 'bold'), bg=WHITE, fg=BLUE)
+        self.heading.place(x=80, y=60, width=400, height=30)
+
+        # Left side image
+        self.side_image = Image.open('./images/left.png')
+        photo = ImageTk.PhotoImage(self.side_image)
+        self.side_image_label = Label(self.lgn_frame, image=photo, bg=WHITE)
+        self.side_image_label.image = photo
+        self.side_image_label.place(x=5, y=100)
+
+        # Sign in image
+        self.sign_in_image = Image.open('./images/signin.png')
+        photo = ImageTk.PhotoImage(self.sign_in_image)
+        self.sign_in_image_label = Label(self.lgn_frame, image=photo, bg=WHITE)
+        self.sign_in_image_label.image = photo
+        self.sign_in_image_label.place(x=650, y=130)
+
+        self.sign_in_label = Label(self.lgn_frame, text='Sign In', 
+                                   font=('yu gothic ui', 17, 'bold'), bg=WHITE, fg=BLUE)
+        self.sign_in_label.place(x=650, y=240)
+
+        # Username
+        self.username_label = Label(self.lgn_frame, text='Username', bg=WHITE, fg=DARK_GRAY, 
+                                    font=('yu gothic ui', 13, 'bold'))
+        self.username_label.place(x=550, y=300)
+
+        self.username_entry = Entry(self.lgn_frame, highlightthickness=0, relief=FLAT, bg=WHITE, 
+                                    fg=DARK_GRAY, font=('yu gothic ui', 13, 'bold'))
+        self.username_entry.place(x=580, y=335, width=270)
+
+        self.username_line = Canvas(self.lgn_frame, width=300, height=2.0, highlightthickness=0, bg=LIGHT_GRAY)
+        self.username_line.place(x=550, y=359)
+
+        # Username Icon
+        self.username_icon = Image.open('./images/username_icon.png')
+        photo = ImageTk.PhotoImage(self.username_icon)
+        self.username_icon_label = Label(self.lgn_frame, image=photo, bg=WHITE)
+        self.username_icon_label.image = photo
+        self.username_icon_label.place(x=550, y=332)
+
+        # Login button
+        self.lgn_button = Image.open('./images/btn1.png')
+        photo = ImageTk.PhotoImage(self.lgn_button)
+        self.lgn_button_label = Label(self.lgn_frame, image=photo, bg=WHITE)
+        self.lgn_button_label.image = photo
+        self.lgn_button_label.place(x=550, y=450)
+        self.login = Button(self.lgn_button_label, text='LOGIN', font=("yu gothic ui", 13, "bold"), width=20, bd=0,
+                            bg=BLUE, cursor='hand2', activebackground=BLUE, fg=WHITE, highlightthickness=0, command=self.login_action)
+        self.login.place(x=20, y=10)
+
+        # Forgot password
+        self.forgot_button = Button(self.lgn_frame, text="Forgot Password?",
+                                    font=("yu gothic ui", 13, "bold underline"), fg=BLUE, relief=FLAT, highlightthickness=0,
+                                    activebackground=WHITE, borderwidth=0, background=WHITE, cursor="hand2")
+        self.forgot_button.place(x=610, y=510)        
+
+        # Sign up
+        self.sign_label = Label(self.lgn_frame, text='No account yet?', font=("yu gothic ui", 11, "bold"),
+                                relief=FLAT, borderwidth=0, background=WHITE, fg=DARK_GRAY, highlightthickness=0,)
+        self.sign_label.place(x=585, y=560)
+
+        self.signup_img = ImageTk.PhotoImage(file='./images/register.png')
+        self.signup_button_label = Button(self.lgn_frame, image=self.signup_img, bg=WHITE, cursor="hand2",
+                                          borderwidth=0, background=WHITE, activebackground=WHITE, highlightthickness=0,command=self.sign_up)
+        self.signup_button_label.place(x=725, y=550, width=100, height=35)
+
+        # Password
+        self.password_label = Label(self.lgn_frame, text='Password', bg=WHITE, fg=DARK_GRAY, 
+                                    font=('yu gothic ui', 13, 'bold'))
+        self.password_label.place(x=550, y=380)
+
+        self.password_entry = Entry(self.lgn_frame, highlightthickness=0, relief=FLAT, bg=WHITE, 
+                                    fg=DARK_GRAY, font=('yu gothic ui', 13, 'bold'), show='*')
+        self.password_entry.place(x=580, y=416, width=244)
+
+        self.password_line = Canvas(self.lgn_frame, width=300, height=2.0, highlightthickness=0, bg=LIGHT_GRAY)
+        self.password_line.place(x=550, y=440)
+
+        # Password Icon
+        self.password_icon = Image.open('./images/password_icon.png')
+        photo = ImageTk.PhotoImage(self.password_icon)
+        self.password_icon_label = Label(self.lgn_frame, image=photo, bg=WHITE)
+        self.password_icon_label.image = photo
+        self.password_icon_label.place(x=550, y=414)
+
+        # Show/hide password
+        self.show_image = ImageTk.PhotoImage(file='./images/show.png')
+        self.hide_image = ImageTk.PhotoImage(file='./images/hide.png')   
+        self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show_password, relief=FLAT,
+                                  activebackground=WHITE, borderwidth=0, background=WHITE, cursor="hand2")
+        self.show_button.place(x=860, y=420)
+
+        ######
+        self.gui_done = True
+        self.root.protocol("WM_DELETE_WINDOW", self.stop)
+        self.root.mainloop()
+
+    def show_password(self):
+        self.hide_button = Button(self.lgn_frame, image=self.hide_image, command=self.hide_password, relief=FLAT,
+                                  activebackground=WHITE, borderwidth=0, background=WHITE, cursor="hand2")
+        self.hide_button.place(x=860, y=420)
+        self.password_entry.config(show='')
+
+    def hide_password(self):
+        self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show_password, relief=FLAT,
+                                  activebackground=WHITE, borderwidth=0, background=WHITE, cursor="hand2")
+        self.show_button.place(x=860, y=420)
+        self.password_entry.config(show='*')
+    
+    def login_action(self):
+        while self.running:
+            try:
+                username = self.username_entry.get()
+                password = self.password_entry.get()
+                self.user_info = str(["LOG-IN", username, password])
+                self.client.send(self.user_info.encode(FORMAT))
+
+                response = self.client.recv(BUFFER_SIZE).decode(FORMAT)
+                messagebox.showinfo(None, response)
+
+                if response == "Login successful!":
+                    if self.gui_done:
+                        break
+                else:
+                    break
+            except ConnectionAbortedError:
+                break
+            except:
+                print("[ERROR]: An error occurred!")
+                self.client.close()
+                break
+
+    def sign_up(self):
+        self.master = Toplevel()
+        SignUp(self.master)
+    
+    def stop(self):
+        self.running = False
+        self.root.destroy()
+        self.client.close()
+        exit(0)
+
+client = Client(IP, PORT)
--- server.py
+++ server.py
@@ -1,23 +1,131 @@
 import socket
+import threading
+import sys
+import re
+import json
 
-# define host and port server listening
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 20  # number bytes that server can received once time
+# Create Socket (TCP) Connection
+IP = '127.0.0.1'
+PORT = 5005
+BUFFER_SIZE = 2048
+FORMAT = 'ascii'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)  # 1 is number of conection appected
-print("Server listening on port", TCP_PORT)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-c, addr = s.accept()  # c: connection, addr: addr of request/client
-print('Connect from:', addr)
+try: 
+    server.bind((IP, PORT))
+except socket.error as s_e:
+    print("Error creating socket: ", s_e)
+    sys.exit(1)
 
-# client send to server
-while 1:
-    data = c.recv(BUFFER_SIZE)
-    if not data:
-        break
-    print("Server received data:", data.decode())
-    c.send(data)
-c.close()
+server.listen()
+
+print("==== SEVER RUNNING ====")
+print(f"Server {IP} listening on port {PORT}")
+print("Waiting for clients...")
+
+clients = []
+users = []
+
+# Function : For each client
+special_char = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+def checkSpecialChar(string):
+    if (special_char.search(string)):
+        return True
+    return False
+
+
+def check(username, password):
+    if len(username) < 5:
+        return False
+    if re.findall('[A-Z]', username) or checkSpecialChar(username):
+        return False
+    if len(password) < 3:
+        return False
+    return True
+
+def handle(client):
+    while True:
+        try:
+            user_data = client.recv(BUFFER_SIZE).decode(FORMAT)
+            user_data = eval(user_data)
+            mode = user_data[0]
+            if mode == "LOG-IN":
+                file = open("./user.json")
+                users_data = json.load(file)
+                user_exist = False
+                username = user_data[1]
+                password = user_data[2]
+                if check(username, password):
+                    for user in users_data:
+                        if user["username"] == username:
+                            if user["password"] == password:
+                                users.append([username, password])
+                                json_obj = json.dumps(users_data, indent=4)
+                                with open("user.json", "w") as fo:
+                                    fo.write(json_obj)
+                                client.send("Login successful!".encode(FORMAT))
+                            else:
+                                client.send("Wrong password!".encode(FORMAT))
+                            user_exist = True
+                            break
+                else:
+                    client.send("Invalid username!".encode(FORMAT))
+                if not user_exist:
+                    client.send("User does not exist!".encode(FORMAT))
+                file.close()
+            elif mode == "SIGN-UP":
+                print("Receive sign up")
+                file = open("./user.json")
+                users_data = json.load(file)
+                user_exist = False
+                username = user_data[1]
+                password = user_data[2]
+                confirm_pw = user_data[3]
+                if check(username, password):
+                    user_exist = False
+                    for user in users_data:
+                        if (user["username"] == username):
+                            client.send("User is already exist".encode(FORMAT))
+                            user_exist = True
+                            break
+                    if not(user_exist):
+                        if confirm_pw == password:
+                            user_info = {"username": username,
+                                            "password": password}
+                            # init new user's note
+                            users_data.append(user_info)
+                            client.send("Register successfully".encode(FORMAT))
+                        else:
+                            client.send(
+                                "Password is not matched".encode(FORMAT))
+                else:
+                    client.send("Invalid username or password".encode(FORMAT))
+                json_obj = json.dumps(users_data, indent=4)
+                with open("user.json", "w") as fo:
+                    fo.write(json_obj)
+                file.close()            
+        except:
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                users.remove(users[index])
+                client.close()
+
+def receive():
+    ThreadCount = 0
+    while True:
+        client, address = server.accept()
+
+        print(f"Connected with {str(address)}!")
+        print("Client:", client.getsockname())
+
+        clients.append(client)
+
+        client_handler = threading.Thread(target=handle, args=(client,))
+        client_handler.start()
+        ThreadCount += 1
+        print("Connection Request: " + str(ThreadCount))
+
+receive()
